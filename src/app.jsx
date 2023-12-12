423c732e915d1d3769ee4e883a0fd13806c3cd4a
Neftali Yagua <despacho@neftaliyagua.com>
2023-12-11 19:07:55 -0500

    Fixes

--- src/app.jsx
+++ src/app.jsx
@@ -37,7 +37,7 @@ export class Application extends React.Component {
     render() {
         return (
             <Card>
-                <CardTitle>Starter Kit</CardTitle>
+                <CardTitle>Databases</CardTitle>
                 <CardBody>
                     <Alert
                         variant="info"
